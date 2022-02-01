f19f8093dc34143e4ba711a2d9ef5bdf1f603281
pavangabani <pavan.gabani@gmail.com>
2022-02-01 19:09:06 +0530

    1 feb

---
+++ src/socketprogramming/datagram/onetoone/client.java
@@ -0,0 +1,20 @@
+package socketprogramming.datagram.onetoone;
+
+import java.io.IOException;
+import java.net.*;
+
+public class client {
+    public static void main(String[] args) throws IOException {
+        DatagramSocket datagramSocket=new DatagramSocket();
+        InetAddress ip=InetAddress.getLocalHost();
+
+        String message="Hello Server";
+        DatagramPacket datagramPacket=new DatagramPacket(message.getBytes(),message.length(),ip,8888);
+
+        datagramSocket.send(datagramPacket);
+
+        datagramSocket.receive(datagramPacket);
+        String serverMessage=new String(datagramPacket.getData(),0,datagramPacket.getLength());
+        System.out.println(serverMessage);
+    }
+}
---
+++ src/socketprogramming/datagram/onetoone/server.java
@@ -0,0 +1,26 @@
+package socketprogramming.datagram.onetoone;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class server {
+    public static void main(String[] args) throws IOException {
+        DatagramSocket datagramSocket=new DatagramSocket(8888);
+
+        byte[] message=new byte[1024];
+        DatagramPacket datagramPacket=new DatagramPacket(message,message.length);
+        datagramSocket.receive(datagramPacket);
+
+        String clientMessage=new String(datagramPacket.getData(),0,datagramPacket.getLength());
+        System.out.println(clientMessage);
+
+        InetAddress ip=datagramPacket.getAddress();
+        int port=datagramPacket.getPort();
+
+        String reply="hi am server";
+        datagramPacket=new DatagramPacket(reply.getBytes(),reply.length(),ip,port);
+        datagramSocket.send(datagramPacket);
+    }
+}
